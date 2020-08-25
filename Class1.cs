512118560a79b5d847f15fbd5fc67cf86f718b69
kumu1997 <guru.vakkalagadda@gmail.com>
2020-08-24 23:03:48 -0700

    create xml

---
+++ Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Program
+    {
+        public void Test()
+        //public static void Main()
+        {
+            XDocument variableName = new XDocument(
+            new XElement("Root",
+            new XElement("Child1", "Data1")
+            )
+            );
+
+            variableName.Save("C:/Users/Guru/Documents/testxml/dxml1.xml");
+        }
+    }
+}
